23deeac6c46a224e2ea8a23bf6d1c14c6fd4d19e
GiRang Choi <choigirang@kakao.com>
2023-03-05 23:51:18 +0900

    feat: 폰트 및 module 수정

--- src/index.js
+++ src/index.js
@@ -1,14 +1,13 @@
 import React from "react";
-import ReactDOM from "react-dom";
 import reportWebVitals from "./reportWebVitals";
-import { createStore, applyMiddleware, compose } from "redux";
+import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
 import "./index.css";
 import App from "./App";
-import rootReducer from "./reducers";
-const store = createStore(rootReducer);
+import store from "./store/store";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement);
 root.render(
   <Provider store={store}>
     <App />
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,17 @@
 import { combineReducers } from "redux";
-import ToDoList from "./ToDoList";
+import { toggleReject } from "./rejectReducer";
+import { togglePaper } from "./paperReducer";
+import { todoReducer } from "./todoReducer";
 
 // 여러 reducer를 사용하는 경우 reducer를 하나로 묶어주는 메소드입니다.
 // store에 저장되는 리듀서는 오직 1개입니다.
 const rootReducer = combineReducers({
-  ToDoList,
+  toggleReject,
+  togglePaper,
+  todoReducer,
 });
+// 합칠 때 카테고리별로 분류가 되는데,
+// setReject라는 이름으로 합친 거임
+// store.setReject.reject
 
 export default rootReducer;
