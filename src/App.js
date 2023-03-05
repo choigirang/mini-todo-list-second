90f20725bdcadcc7ba4828789811be9d0a23d814
GiRang Choi <choigirang@kakao.com>
2023-03-05 20:53:33 +0900

    feat: background 추가

--- src/App.js
+++ src/App.js
@@ -14,6 +14,8 @@ function App() {
           <Route path="/Home" element={<Home />}></Route>
         </Routes>
       </BrowserRouter>
+      <Face src="./sunFace.png" />
+      <Sun src="./sun.png" />
       <FamilyImg src="./family.png" />
       <House src="./house.png"></House>
     </div>
@@ -35,5 +37,34 @@ const House = styled.img`
   right: 2rem;
   top: 7rem;
   width: 40rem;
+  z-index: -1;
+`;
+
+const Sun = styled.img`
+  position: absolute;
+  width: 25rem;
+  z-index: -2;
+  top: 0rem;
+  animation: roll 8s infinite ease-in;
+
+  @keyframes roll {
+    0% {
+      transform: rotate(0deg);
+    }
+    50% {
+      transform: rotate(360deg);
+      scale: 0.5;
+    }
+    100% {
+      transform: rotate(0deg);
+    }
+  }
+`;
+
+const Face = styled.img`
+  position: absolute;
+  left: 5rem;
+  top: 4rem;
+  width: 15rem;
   z-index: -1;
 `;
