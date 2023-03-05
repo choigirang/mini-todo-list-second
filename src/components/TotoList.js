43af458408440f73cd619bc5bda7198e3c4e0e4c
GiRang Choi <choigirang@kakao.com>
2023-03-05 23:50:45 +0900

    feat: reducer 받아와서 컴포넌트 추가하기
    map 사용하여 기존의 값 변화,
    todo 추가는 미완

--- src/components/TotoList.js
+++ src/components/TotoList.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { useSelector, useDispatch } from "react-redux";
-import { todoReducer } from "../reducers/todoReducer";
+import { toggleCheck } from "../actions/index";
 import styled from "styled-components";
 
 const TotoList = () => {
@@ -8,12 +8,37 @@ const TotoList = () => {
   const state = useSelector((state) => state.todoReducer.todos);
   //   const { todos } = state;
   //   console.log(state);
+  const handleCheck = (e) => {
+    dispatch(toggleCheck(e));
+  };
+
   return (
     <Container>
       {state.map((item) => {
         return (
           <div key={item.id} className="todoBox">
-            <div className="room">{item.room}</div>
+            <div className="textBox">
+              {!item.checked ? (
+                <img
+                  src={`./uncheckBox${Math.floor(
+                    Math.random() * (4 - 0) + 1
+                  )}.png`}
+                  onClick={() => handleCheck(item.id)} // item id 바로 넘겨주기 payload
+                ></img>
+              ) : (
+                <img
+                  src={`./checkBox${Math.floor(
+                    Math.random() * (4 - 0) + 1
+                  )}.png`}
+                ></img>
+              )}
+              <div className="room">{item.room}</div>
+              <div className="clean">{item.clean}</div>
+              <div className="clean">({item.during})</div>
+            </div>
+            <img
+              src={`./piece${Math.floor(Math.random() * (4 - 0) + 1)}.png`}
+            />
           </div>
         );
       })}
@@ -24,18 +49,47 @@ const TotoList = () => {
 export default TotoList;
 
 const Container = styled.div`
-  width: 30%;
-  height: 90%;
-  background-color: #fff;
+  width: 40%;
+  height: 100%;
   display: flex;
   flex-direction: column;
   justify-content: start;
-  padding: 1rem;
+  padding: 0.7rem 0.2rem;
+  padding-top: 5rem;
+
+  background-image: url("./paper.png");
+  background-size: 100% 100%;
 
   > .todoBox {
-    /* background-color: black; */
     width: 100%;
     height: 10%;
     box-sizing: border-box;
+    position: relative;
+
+    > .textBox {
+      width: 70%;
+      display: flex;
+      justify-content: space-around;
+      font-size: 1.5rem;
+      position: absolute;
+      top: 50%;
+      left: 50%;
+      transform: translate(-50%, -50%);
+
+      > img {
+        width: 2rem;
+        cursor: pointer;
+      }
+
+      > .clean {
+        font-size: 1.2rem;
+        line-height: 1.8rem;
+      }
+    }
+
+    > img {
+      width: 100%;
+      height: 100%;
+    }
   }
 `;
