accf7f32b194ff36e88051f7f5ab2aa99c79ae45
GiRang Choi <choigirang@kakao.com>
2023-03-07 06:21:48 +0900

    feat: check 값 변경하기
    reducer작성

--- src/reducers/todoReducer.js
+++ src/reducers/todoReducer.js
@@ -1,15 +1,13 @@
-import { ADD_TO_DO } from "../actions/index";
-
-const num = 1;
+import { ADD_TO_DO, TOGGLE_CHECK } from "../actions/index";
 
 export const TodoList = {
   todos: [
     {
       id: new Date(),
       room: "작은 방",
-      during: "5분",
+      during: 5,
       checked: false,
-      clean: "청소기",
+      clean: "청소기 돌리기",
     },
   ],
 };
@@ -19,6 +17,18 @@ export const todoReducer = (state = TodoList, action) => {
     case ADD_TO_DO:
       return Object.assign({}, state, {
         todos: [...state.todos, action.payload],
+      });
+
+    case TOGGLE_CHECK:
+      let idx = state.todos.findIndex((el) => el.id === action.payload);
+      state.todos[idx].checked = !state.todos[idx].checked; //위에다가 빼내서
+      //TODO
+      return Object.assign({}, state, {
+        todos: [
+          // ...state.todos.slice(0, idx),
+          // ...state.todos.slice(idx + 1),
+          ...state.todos,
+        ],
       });
 
     default:
