617cd82d0de1035746fc367bcef55e4d3b380199
GiRang Choi <choigirang@kakao.com>
2023-03-10 16:49:08 +0900

    feat: reducer추가
    list에 따른 추가 방지, 투듀 삭제

--- src/reducers/todoReducer.js
+++ src/reducers/todoReducer.js
@@ -1,13 +1,17 @@
-import { ADD_TO_DO, TOGGLE_CHECK } from "../actions/index";
+import { ADD_TO_DO, TOGGLE_CHECK, DELETE_TO_DO } from "../actions/index";
+import { CleanList } from "./cleanReducer";
+
+let cleanPercent = CleanList.percent;
 
 export const TodoList = {
   todos: [
     {
       id: new Date(),
-      room: "작은 방",
+      room: "내방",
       during: 5,
       checked: false,
       clean: "청소기 돌리기",
+      // img: url을 받아와서 랜덤으로 생성 ,
     },
   ],
 };
@@ -15,6 +19,7 @@ export const TodoList = {
 export const todoReducer = (state = TodoList, action) => {
   switch (action.type) {
     case ADD_TO_DO:
+      if (state.todos.length >= 9) return state;
       return Object.assign({}, state, {
         todos: [...state.todos, action.payload],
       });
@@ -29,6 +34,11 @@ export const todoReducer = (state = TodoList, action) => {
           // ...state.todos.slice(idx + 1),
           ...state.todos,
         ],
+      });
+
+    case DELETE_TO_DO:
+      return Object.assign({}, state, {
+        todos: [...state.todos.filter((el) => el.id !== action.payload)],
       });
 
     default:
