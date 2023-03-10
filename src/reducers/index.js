2c8cf63b40cc0f5bca51225e02b490fa9a7eed77
GiRang Choi <choigirang@kakao.com>
2023-03-10 16:49:30 +0900

    feat: reducer 추가

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,6 +2,7 @@ import { combineReducers } from "redux";
 import { toggleReject } from "./rejectReducer";
 import { togglePaper } from "./paperReducer";
 import { todoReducer } from "./todoReducer";
+import { doClean } from "./cleanReducer";
 
 // 여러 reducer를 사용하는 경우 reducer를 하나로 묶어주는 메소드입니다.
 // store에 저장되는 리듀서는 오직 1개입니다.
@@ -9,6 +10,7 @@ const rootReducer = combineReducers({
   toggleReject,
   togglePaper,
   todoReducer,
+  doClean,
 });
 // 합칠 때 카테고리별로 분류가 되는데,
 // setReject라는 이름으로 합친 거임
